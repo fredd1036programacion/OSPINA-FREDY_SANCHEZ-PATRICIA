aa3c14b331d5ab86ca23a139d0e238d5a5fd27b0
fredy <fredd1036programacion@hotmail.com>
2023-08-09 23:12:41 -0500

    avance2

---
+++ OSPINA-FREDY_SANCHEZ-PATRICIA/OSPINA-FREDY_SANCHEZ-PATRICIA/src/com/backend/integrador/dao/imp/OdontologDaoEnMemoria.java
@@ -0,0 +1,36 @@
+package com.backend.integrador.dao.imp;
+
+import com.backend.integrador.dao.IDao;
+import com.backend.integrador.entity.Odontologo;
+import org.apache.log4j.Logger;
+
+import java.util.List;
+
+public class OdontologDaoEnMemoria implements IDao<Odontologo> {
+
+    private static final Logger LOGGER = Logger.getLogger(OdontologDaoEnMemoria.class);
+    private List<Odontologo> odontologoslista;
+
+    public OdontologDaoEnMemoria(List<Odontologo> odontologoslista) {
+        this.odontologoslista = odontologoslista;
+    }
+
+    @Override
+    public Odontologo registrar(Odontologo odontologo) {
+        odontologoslista.add(odontologo);
+        LOGGER.info("Odontologo guardado: " + odontologo);
+        return odontologo;
+    }
+
+    @Override
+    public Odontologo buscarPorId(int id) {
+        Odontologo odontologoBuscado = odontologoslista.get(id -1);
+        if(odontologoBuscado != null) LOGGER.info("El Odontologo de ID " +id + "se encuentra en la Base de Datos " + odontologoBuscado);
+        return odontologoBuscado;
+    }
+
+    @Override
+    public List<Odontologo> listarTodos() {
+        return null;
+    }
+}
