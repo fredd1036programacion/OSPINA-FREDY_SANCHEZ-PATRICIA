d30601721d9a2f74a53b5442caca5a9ae23148fd
fredy <fredd1036programacion@hotmail.com>
2023-08-09 21:39:00 -0500

    cambio

--- OSPINA-FREDY_SANCHEZ-PATRICIA/OSPINA-FREDY_SANCHEZ-PATRICIA/src/com/backend/integrador/entity/Odontologo.java
+++ OSPINA-FREDY_SANCHEZ-PATRICIA/OSPINA-FREDY_SANCHEZ-PATRICIA/src/com/backend/integrador/entity/Odontologo.java
@@ -19,5 +19,36 @@ public class Odontologo {
         this.nombre = nombre;
         this.apellido = apellido;
     }
-    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getNumeroMatricula() {
+        return numeroMatricula;
+    }
+
+    public void setNumeroMatricula(int numeroMatricula) {
+        this.numeroMatricula = numeroMatricula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
 }
